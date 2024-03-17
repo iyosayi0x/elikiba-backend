e382e88be3decb008b4121043c744bd7901b3973
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2024-03-17 18:11:11 +0100

    chore: lists posts

--- src/routes.py
+++ src/routes.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from .models import Article, engine
+
 
 posts_router = APIRouter()
 
 
 @posts_router.get("/posts/")
 def list_posts():
+    with Session(engine) as session:
+        statement = select(Article)
+        rows = session.execute(statement).all()
+        print(rows)
     return {
         "posts": "works"
     }
