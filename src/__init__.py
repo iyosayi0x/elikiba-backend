04398e6ff563a790761c793dc104aa51fac42b12
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2024-03-19 18:16:29 +0100

    feat: set language to english

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,18 @@
 from fastapi_amis_admin.admin.site import AdminSite
 from fastapi_amis_admin.admin import ModelAdmin
 from fastapi_amis_admin.admin.settings import Settings
+from fastapi_amis_admin import i18n
 
 from src.config import Config
 from .models import Article
 from main import app
 
+
 # admin site
+i18n.set_language(language='en_US')
 site = AdminSite(
     settings=Settings(
         database_url=Config.DATABASE_URI,
-        language="en_US",
         debug=Config.DEBUG,
         site_title="Elikiba Admin",
     )
@@ -21,11 +23,23 @@ site = AdminSite(
 class ArticleAdmin(ModelAdmin):
     page_schema = 'Articles'
     model = Article
+    page_path = "/articles"
+    bind_model = True
+    update_fields = [
+        Article.title,
+        Article.is_headline,
+        Article.type,
+        Article.author,
+        Article.content,
+        Article.excerpt,
+        Article.read_minutes,
+        Article.tags,
+    ]
     list_display = [
         Article.id,
         Article.title,
         Article.slug,
-        Article.created_at
+        Article.created_at,
     ]
     ordering = "created_at"
     list_per_page = 20
--- src/schemas.py
+++ src/schemas.py
@@ -10,12 +10,15 @@ class ArticleSchema(BaseModel):
         default_factory=uuid.uuid4, primary_key=True, nullable=False)
     is_headline: bool = Field(default=False)
     title: str = Field(nullable=False)
-    slug: str = Field(nullable=False)
+    slug: str = Field(nullable=True)
     type: str = Field(default="article")
     author: str = Field(nullable=False)
     content: str = Field(nullable=False)
     excerpt: str = Field(nullable=False)
     read_minutes: int = Field(nullable=False)
-    tags: str = Field(nullable=False)
+    tags: str = Field(
+        nullable=False,
+        description="Article tags, separate with comma(,) for multiple tags"
+    )
     created_at: datetime = Field(nullable=False)
     last_updated_at: datetime = Field(nullable=False)
