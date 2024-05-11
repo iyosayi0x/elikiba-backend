fe3230016e0b82b41045fbee22faa8d32235ba6d
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2024-05-11 16:31:03 +0100

    chore: setup backend

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,19 @@ from elikiba.models import BaseModel
 # Create your models here.
 
 
+class Tag(BaseModel):
+    name = models.CharField(max_length=20, unique=True)
+
+
+class Author(BaseModel):
+    name = models.CharField(max_length=40)
+
+
 class Article(BaseModel):
-    pass
-
-
-class Tags(BaseModel):
-    pass
+    slug = models.SlugField(max_length=100)
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
+    content = models.TextField()
+    exert = models.CharField(max_length=200)
+    read_minutes = models.PositiveIntegerField()
+    tags = models.ManyToManyField(Tag, related_name="tags")
